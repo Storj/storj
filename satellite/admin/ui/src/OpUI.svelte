bf3a58bb766497adb196bab2429e35712fe7ee4c
Ivan Fraixedes <ivan@fraixed.es>
2021-03-23 12:20:40 +0100

    WIP: Creating the UI from the API object
    Change-Id: Ic470d7c89983a5fad1f6d8cf286d16013b6e3dc1

---
+++ satellite/admin/ui/src/OpUI.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  // TODO: write a component that take the list of parameters and types and the
+  // function to call (i.e. reference to a method of the API) and render a UI
+  // (input texts fields with specify type, etc., and submit button)
+  // In the future it will be good to read a Swagger operation API from JSON format
+  // and render the UIR
+</script>
